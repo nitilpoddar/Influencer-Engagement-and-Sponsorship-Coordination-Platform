9a7df92d9372cdfb7efad25cc662612625b5353c
Nitil Poddar <npoddar.zjef@gmail.com>
2024-09-09 18:33:28 +0530

    Resetting everything and starting from scratch

--- controllers/user_blueprint_test.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('user', __name__, url_prefix='/user')
-
-@bp.route('/register', methods=['GET'])
-def register():
-    return render_template('register_user.html')
